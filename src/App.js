0c0ddcd8b02c5bb3c292b1ba4fa3c38da313c5db
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-10-31 16:54:26 +0300

    1. Install material-ui. Redesign the page using components from material-ui; 2. Add autofocus to the text field when opening a page and after sending a message; 3. Add an "array of chats" - an array of objects with fields name and id. Add a chat list to the left of the message list; 4. Make changes to the ".gitignore" file; 5. Make small changes to the visual design.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { List, ListItem, Box, InputBase, IconButton } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Send } from '@material-ui/icons';
 import {CHAT_LIST as chatList} from './data/chat list'
-import './styles/App.css';
+
+const useStyles = makeStyles({
+  allChatsListItem: {
+    marginBottom: '0.1vh',
+    color: '#dddddd',
+    fontSize: '36px',
+  },
+  chat: {
+    width: '60vw',
+    minHeight: '77vh',
+    margin: '0',
+    backgroundColor: '#88bbdd',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: '5vh 5vw',
+    borderRadius: '5vw',
+  },
+  form: {
+    height: '5vh',
+    width: '100%',
+    minHeight: '25px',
+    display: 'flex',
+    borderBottom: '1px solid #333333'
+  },
+  input: {
+    width: '20px',
+    flexGrow: 1,
+    borderRight: 'none',
+    outline: 'none',
+  },
+  chatList: {
+    width: '100%',
+  },
+  chatListItem: {
+    marginTop: '1vh',
+    color: '#555555',
+    fontSize: '24px',
+  },
+});
 
 export const App = () => {
+  const classes = useStyles();
+
   const [messageList, setMessageList] = useState([]);
   const [value, setValue] = useState('');
+
+  const refInput = useRef(null);
 
   const onSaveValueFromInput = (event) => {
     setValue(event.target.value);
@@ -38,6 +85,12 @@ export const App = () => {
     }
   };
 
+  useEffect((() => {
+    if (messageList.length === 0 || messageList[messageList.length - 1].author !== 'bot') {
+      refInput.current.focus();
+    }
+  }),[messageList])
+
   useEffect(() => {
       if (messageList.length !== 0) {
         const timerId = setTimeout(() => {
@@ -58,23 +111,23 @@ export const App = () => {
   }, [messageList]);
 
   return (
-    <div>
-      <div>
+    <Box display="flex" justifyContent="space-between" mx='10vw' p={1} bgcolor="trancend" color="white">
+      <List component="nav">
         {
-          chatList.map((item) => <li key={item.id}>{item.name}</li>)
+          chatList.map((item) => <ListItem className={classes.allChatsListItem} button key={item.id}>{item.name}</ListItem>)
         }
-      </div>
-      <div className='app'>
-        <form className='app__form' onSubmit={onSubmit}>
-          <input className='app__form_input' type="text" onChange={onSaveValueFromInput} value={value} />
-          <button className='app__form_btn'>Отправить</button>
-        </form>
-        <ul className='app__ul'>
-            {
-              messageList.map((item) => <li className='app__ul_li' key={item.id}>{item.author}: {item.text}</li>)
-            }
-        </ul>
-      </div>
-    </div>
+      </List>
+      <Box className={classes.chat}>
+        <List className={classes.chatList}>
+          {
+            messageList.map((item) => <ListItem className={classes.chatListItem} key={item.id}>{item.author}: {item.text}</ListItem>)
+          }
+        </List>
+        <Box className={classes.form} component='form' onSubmit={onSubmit}>
+          <InputBase className={classes.input} inputRef={refInput} placeholder="Сообщение" label="Сообщение" type="text" onChange={onSaveValueFromInput} value={value} />
+          <IconButton type='submit'><Send /></IconButton>
+        </Box>
+      </Box>
+    </Box>
   );
 };
