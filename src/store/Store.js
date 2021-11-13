8014c1dc1e4f86ae38bc50eea1bdd3fbf15252b9
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-11-13 21:41:09 +0300

    1. Install the "redux-persist" library; 2. Synchronize local storage with the Store and connect it to the application.

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,5 +1,7 @@
 import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { profileReducer } from './Profile/Reducer';
 import { chatListReducer } from './ChatList/Reducer';
 import { chatsListReducer } from './ChatsList/Reducer';
@@ -12,7 +14,16 @@ const rootReducer = combineReducers({
     chatsListReducer: chatsListReducer,
 });
 
+const persistConfig = {
+    key: 'root',
+    storage,
+};
+
+const persistedReduser = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-    rootReducer, 
+    persistedReduser, 
     composeEnhancers(applyMiddleware(thunk))
 );
+
+export const persistor = persistStore(store);
