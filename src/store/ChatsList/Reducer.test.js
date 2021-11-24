6203f95409b533c59c0ab0444a77b34126c331d6
Juris-Berkulis <krztfjrbhm1@jmail.com>
2021-11-25 00:43:07 +0300

    1. Add one file with tests for the Reducer; 2. Fix a bug in Aktion; 3. Make minor adjustments to the Reducer code.

---
+++ src/store/ChatsList/Reducer.test.js
@@ -0,0 +1,96 @@
+import { addInChatsListAction, removeFromChatsListAction } from "./Action";
+import { chatsListReducer, initialState } from "./Reducer";
+
+describe('check reducer', () => {
+    it('check reducer without action', () => {
+        const result = chatsListReducer();
+        expect(result).toEqual(initialState);
+    });
+
+    it("check reducer when adding a chat if chats weren't", () => {
+        const result = chatsListReducer(undefined, addInChatsListAction({
+                id: '12345678',
+                name: 'newName',
+            }));
+        expect(result).toEqual({
+            chats: {
+                newName: {
+                id: '12345678',
+                name: 'newName',
+                }
+            }
+        });
+    });
+
+    it('check reducer when remove a chat if this is last chat', () => {
+        const result = chatsListReducer({
+            chats: {
+                newName: {
+                id: '12345678',
+                name: 'newName',
+                },
+            }
+        }, removeFromChatsListAction('newName'));
+        expect(result).toEqual({
+            chats: {},
+            deletedChatName: 'newName',
+        });
+    });
+
+    it('check reducer when adding a chat if also chats are', () => {
+        const result = chatsListReducer(
+            {
+                chats: {
+                    newName1: {
+                        id: '12345678',
+                        name: 'newName1',
+                    },
+                },
+                deletedChatName: 'delName',
+            } , addInChatsListAction(
+                {
+                    id: '55555555',
+                    name: 'newName2',
+                }
+            )
+        );
+        expect(result).toEqual({
+            chats: {
+                newName1: {
+                    id: '12345678',
+                    name: 'newName1',
+                },
+                newName2: {
+                    id: '55555555',
+                    name: 'newName2',
+                },
+            },
+            deletedChatName: 'delName',
+        });
+    });
+
+    it("check reducer when remove a chat if the chat isn't last", () => {
+        const result = chatsListReducer({
+            chats: {
+                newName1: {
+                id: '12345678',
+                name: 'newName1',
+                },
+                newName2: {
+                    id: '55555555',
+                    name: 'newName2',
+                    },
+            },
+            deletedChatName: 'delName3',
+        }, removeFromChatsListAction('newName1'));
+        expect(result).toEqual({
+            chats: {
+                newName2: {
+                    id: '55555555',
+                    name: 'newName2',
+                    }
+            },
+            deletedChatName: 'newName1',
+        });
+    });
+});
